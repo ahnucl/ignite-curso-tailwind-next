3033d75956fb0f9d67b8cbf736983f5361540c51
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-08-22 10:50:59 -0300

    Inicia Sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {},
+    gridTemplateColumns: {
+      app: 'minmax(15rem, 20rem) 1fr',
+    },
   },
   plugins: [],
 }
