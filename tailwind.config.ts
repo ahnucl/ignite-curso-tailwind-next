cdc2f6c10fbd2a70a52cfe204ac317f0eeb8645a
Leonardo Cunha <lalmcunha@gmail.com>
2023-09-09 14:51:03 -0300

    Cria campos do form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,12 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
-    gridTemplateColumns: {
-      app: 'minmax(15rem, 20rem) 1fr',
-      profile: 'max-content 1fr min-content',
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(15rem, 20rem) 1fr',
+        profile: 'max-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
+      },
     },
   },
   plugins: [],
